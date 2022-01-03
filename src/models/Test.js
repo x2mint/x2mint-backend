24dfa832fc3ec2f797f187bf5dad128fe726d6ea
Nguyen Huynh Minh Tien <33385777+TienNHM@users.noreply.github.com>
2022-01-03 10:18:06 +0700

    populate questions and answer when update test

--- src/models/Test.js
+++ src/models/Test.js
@@ -4,66 +4,65 @@ const { formatTimeUTC } = require("../utils/Timezone");
 const testSchema = mongoose.Schema(
   {
     name: {
-    type: String,
-    require: true,
-    default: 0,
-  },
-  creatorId: {
-    type: mongoose.Schema.Types.ObjectId,
-    default: null,
-  },
-  description: {
-    type: String,
-    require: true,
-    default: null,
-  },
-  pin: {
-    type: String,
-    require: true,
-    default: null,
-  },
-  questions: [{
+      type: String,
+      require: true,
+      default: 0,
+    },
+    creatorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      default: null,
+    },
+    description: {
+      type: String,
+      require: true,
+      default: null,
+    },
+    pin: {
+      type: String,
+      require: true,
+      default: null,
+    },
+    questions: [{
       type: mongoose.Schema.Types.ObjectId,
       ref: "questions",
       default: null,
-  }],
+    }],
 
-  startTime: {
-    type: Date,
-    default: new Date()//formatTimeUTC,
-  },
+    startTime: {
+      type: Date,
+      default: new Date()//formatTimeUTC,
+    },
 
-  endTime: {
-    type: Date,
-    default: new Date()//formatTimeUTC,
-  },
-  url: {
-    type: String,
-    default: null,
-  },
-  maxPoints: {
-    type: Number,
-    default: null,
-  },
-
-  questionsOrder: [{
-    type: mongoose.Schema.Types.ObjectId,
-    default: null,
-  }],
-
-  _status: {
-    type: String,
-    default: "",
-  },
-  createdAt: {
-    type: Date,
-    default: new Date()//formatTimeUTC,
-  },
-  updatedAt: {
-    type: Date,
-    default: new Date()//formatTimeUTC,
-  },
-});
+    endTime: {
+      type: Date,
+      default: new Date()//formatTimeUTC,
+    },
+    url: {
+      type: String,
+      default: null,
+    },
+    maxPoints: {
+      type: Number,
+      default: null,
+    },
+    questionsOrder: [{
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "questions",
+      default: null,
+    }],
+    _status: {
+      type: String,
+      default: "",
+    },
+    createdAt: {
+      type: Date,
+      default: new Date()//formatTimeUTC,
+    },
+    updatedAt: {
+      type: Date,
+      default: new Date()//formatTimeUTC,
+    },
+  });
 
 testSchema.method("toJSON", function () {
   const { __v, ...object } = this.toObject();
